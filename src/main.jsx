e7aa45a0ec6c021a3a4fc5fc226325cb0b26d3a3
No4h <no4h1209@gmail.com>
2025-01-23 12:14:45 +0900

    메인페이지 헤더 커밋

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import MainPage from './1. ProjectPage/MainPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <MainPage />
+  </StrictMode>
 )
