c612cd86e40c183d7ce9b3544f0275b12a56b1ac
No4h <no4h1209@gmail.com>
2025-01-23 12:31:54 +0900

    라우터 연결

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import MainPage from './1. ProjectPage/MainPage'
+import ProjWrite from './1. ProjectPage/ProjWrite'
+import FreeLancerPage from './2. FreeLancer/FreeLancerPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/ProjectWrite" element={<ProjWrite />} />
+
+        <Route path="/FreeView" element={<FreeLancerPage />} />
+      </Routes>
+    </Router>
   </StrictMode>
 )
