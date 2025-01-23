1468ef52a0dbaebf4dfb4972594498565897b4f0
ryul00 <gju07059@gmail.com>
2025-01-23 14:31:23 +0900

    1stcommit

---
+++ src/1. ProjectPage/MainPage.jsx
@@ -0,0 +1,27 @@
+import searchIcon from '../images/search.png';
+import './MainPage.css';
+
+function MainPage() {
+
+  return (
+    <div className="MainPage-container">
+      <div className="MainPage-Header">
+        <div className="MainPage-Header-Left">
+          <div className="MainPage-Header-LOGO">P-eeting</div>
+          <div className="MainPage-Header-Search">
+            <div className="MainPage-Header-SearchIcon"><img src={searchIcon} alt="돋보기아이콘" /></div>
+            <input className="MainPage-Header-InputArea" type="text" placeholder="프로젝트 미팅, 피팅"/>
+          </div>
+        </div>
+        <div className="MainPage-Header-Right">
+          <div className="MainPage-Header-Right-ProMatch">프로젝트 매칭</div>
+          <div className="MainPage-Header-Right-FreeMatch">프리랜서 매칭</div>
+          <div className="MainPage-Header-Right-MyProject">마이 프로젝트</div>
+          <div className="MainPage-Header-Right-LoginButton">로그인</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
---
+++ src/1. ProjectPage/ProjWrite.jsx
@@ -0,0 +1,10 @@
+function ProjWrite() {
+
+    return (
+        <div>
+            광렬 시작!
+        </div>
+    )
+}
+
+export default ProjWrite
